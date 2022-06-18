ddf09e85c5ed7cd9850da552ca9eb374a942344a
IlyaKrapivnoy <ilya.krapivnoy@scelex.com>
2022-06-18 22:16:25 +0300

    use context

--- context/index.js
+++ context/index.js
@@ -1,17 +1,17 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext } from "react";
 
 export const Context = createContext();
 
-export const ContextProvider = () => {
-    const [username, setUsername] = useState('');
-    const [secret, setSecret] = useState('');
+export const ContextProvider = (props) => {
+  const [username, setUsername] = useState("");
+  const [secret, setSecret] = useState("");
 
-    const value = {
-        username,
-        setUsername,
-        secret,
-        setSecret
-    }
+  const value = {
+    username,
+    setUsername,
+    secret,
+    setSecret,
+  };
 
-    return <Context.Provider value={value}>{props.children}</Context.Provider>
-}
+  return <Context.Provider value={value}>{props.children}</Context.Provider>;
+};
